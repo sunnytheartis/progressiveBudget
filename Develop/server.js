bbeb2564d528ab3824cdb910f3ec58e74dff0a8a
Sun <solskinartwork@gmail.com>
2021-11-26 20:57:42 -0500

    editing server.js

--- Develop/server.js
+++ Develop/server.js
@@ -3,8 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3000;
+const mongoUrl = process.env.MONGODB_URI || "mongodb://localhost/budget"
 const app = express();
 
 app.use(logger("dev"));
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));    
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(mongoUrl, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
